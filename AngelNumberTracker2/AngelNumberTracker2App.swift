b02c5143b142d4eb6a6e0f6b682f722f10203e32
eeshamoona <eeshamoona@gmail.com>
2022-03-16 21:35:52 -0500

    Initial Commit

---
+++ AngelNumberTracker2/AngelNumberTracker2App.swift
@@ -0,0 +1,17 @@
+//
+//  AngelNumberTracker2App.swift
+//  AngelNumberTracker2
+//
+//  Created by Eesha Moona on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AngelNumberTracker2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
